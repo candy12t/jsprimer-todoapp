d22f5e1109ce0be3211d87369affccc73970bd0e
Takumi Kanada <candy12t@gmail.com>
2021-02-02 19:22:27 +0900

    feat: TodoItem update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { element, render } from "./view/html-util.js";
 
 export class App {
   constructor() {
-    // 1. TodoListの初期化
     this.todoListModel = new TodoListModel();
   }
 
@@ -15,26 +14,34 @@ export class App {
     const containerElement = document.querySelector("#js-todo-list");
     const todoItemCountElement = document.querySelector("#js-todo-count");
 
-    // 2. TodoListModelの状態が更新されたら表示を更新する
+    // TodoListModelの状態が更新されたら表示を更新する
     this.todoListModel.onChange(() => {
-      // TodoリストをまとめるList要素
       const todoListElement = element`<ul />`;
-      // それぞれのTodoItem要素をtodoListElement以下へ追加する
       const todoItems = this.todoListModel.getTodoItems();
+
       todoItems.forEach(item => {
-        const todoItemElement = element`<li>${item.title}</li>`;
+        const todoItemElement = item.completed
+          ? element`<li><input type="checkbox" class="checkbox" checked><s>${item.title}</s></li>`
+          : element`<li><input type="checkbox" class="checkbox">${item.title}</li>`;
+        const inputCheckboxElement = todoItemElement.querySelector(".checkbox");
+
+        inputCheckboxElement.addEventListener("change", () => {
+          this.todoListModel.updateTodo({
+            id: item.id,
+            completed: !item.completed
+          });
+        });
+
         todoListElement.appendChild(todoItemElement);
       });
-      // containerElementの中身をtodoListElementで上書きする
+
       render(todoListElement, containerElement);
-      // アイテム数の表示を更新
       todoItemCountElement.textContent = `Todoアイテム数: ${this.todoListModel.getTotalCount()}`;
     });
 
-    // 3. フォームを送信したら, 新しいTodoItemModelを追加する
+    // フォームを送信したら, 新しいTodoItemModelを追加する
     formElement.addEventListener("submit", (event) => {
       event.preventDefault();
-      // 新しいTodoItemをTodoListへ追加する
       this.todoListModel.addTodo(new TodoItemModel({
         title: inputElement.value,
         completed: false
